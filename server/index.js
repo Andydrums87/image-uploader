5c6e4c6c5470c7162654e2cf3bedf3c10bebbe10
Andy Joseph <neon_4@hotmail.com>
2024-08-22 22:23:23 +0100

    update server

--- server/index.js
+++ server/index.js
@@ -1,12 +1,8 @@
 const express = require('express');
-
 const path = require('path');
 const multer = require('multer');
 const fs = require('fs')
 const cors = require('cors');
-
-
-
 
 const app = express()
 app.use(cors());
