c92f926e266cb85963050406c0650c77a6c6efa5
Andy Joseph <neon_4@hotmail.com>
2024-08-22 11:21:33 +0100

    commit backend

---
+++ server/index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+
+const path = require('path');
+const multer = require('multer');
+const fs = require('fs')
+const cors = require('cors');
+
+
+const app = express()
+app.use(cors());
+
+const storage = multer.diskStorage({
+  destination: "./uploads",
+  filename: (req, file, cb) => {
+    cb(null, `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`)
+  }
+});
+
+const upload = multer({storage: storage})
+
+
+app.post("/upload", upload.array("files"), (req, res) => {
+  const uploadedFiles = req.files
+  console.log(uploadedFiles)
+  if (!uploadedFiles) {
+    return res.status(400).send("No files uploaded!")
+  }
+  for(const file of uploadedFiles) {
+    const filePath = file.path
+    const fileName = file.originalname
+
+
+  try {
+   
+    fs.renameSync(filePath, `./uploads/${fileName}`)
+  } catch (error) {
+    console.log("Error", error)
+    return res.status(500).send("error saving files")
+  }
+}
+  res.status(200).send("Files uploaded successfully")
+});
+
+
+
+app.listen(8080, () => console.log("listening on port 8080"))
